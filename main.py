7d5e06d2949047e65046b458a078a5d7876148d5
Jan <jmolina2300@gmail.com>
2024-12-29 20:18:38 -0500

    Change startup sequence

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from datetime import datetime
 
 
 # Define pin names
-PIN_LED_BLUE = 7
+PIN_DEVICE_POWER = 7
 
 # Set up GPIO mode
 GPIO.setmode(GPIO.BCM)
 
 # Set GPIO pin 17 as an output pin
-GPIO.setup(PIN_LED_BLUE, GPIO.OUT)
+GPIO.setup(PIN_DEVICE_POWER, GPIO.OUT)
 
 
 
@@ -18,21 +18,28 @@ GPIO.setup(PIN_LED_BLUE, GPIO.OUT)
 def main():
     current_time = get_current_time()
     print('Script started at ',current_time)
+	
+	GPIO.output(PIN_DEVICE_POWER, GPIO.LOW)
+	time.sleep(2)
+	GPIO.output(PIN_DEVICE_POWER, GPIO.HIGH)
+	time.sleep(2)
     try:
         while True:
-            blink()
+            pass
     except KeyboardInterrupt:
         pass
     finally:
         GPIO.cleanup()  # Clean up GPIO pins
 
+
 def get_current_time():
     return datetime.now().replace(microsecond=0).isoformat()
 
+
 def blink():
-    GPIO.output(PIN_LED_BLUE, GPIO.HIGH)   # Turn on LED
+    GPIO.output(PIN_DEVICE_POWER, GPIO.HIGH)   # Turn on
     time.sleep(1)
-    GPIO.output(PIN_LED_BLUE, GPIO.LOW)    # Turn off LED
+    GPIO.output(PIN_DEVICE_POWER, GPIO.LOW)    # Turn off
     time.sleep(1)
 
 
